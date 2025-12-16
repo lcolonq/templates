39e45d0d9e3b188d595db6c836a63fb75d664902
LLLL Colonq <llll@colonq>
2025-12-15 21:44:49 -0500

    game-teleia: Fix nix build

--- game-teleia/flake.nix
+++ game-teleia/flake.nix
@@ -8,10 +8,16 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      game = inputs.teleia.native.build ./. "game";
+      game = inputs.teleia.native.build ./. "game_lib";
+      wasm = inputs.teleia.wasm.build ./. "game_lib";
     in {
       packages.${system} = {
-        inherit game;
+        inherit game wasm;
+        default = game;
+      };
+      applications.${system}.default = {
+        type = "app";
+        program = "${game}/bin/game";
       };
       devShells.${system}.default = inputs.teleia.shell.overrideAttrs (final: prev: {
         buildInputs = prev.buildInputs;
